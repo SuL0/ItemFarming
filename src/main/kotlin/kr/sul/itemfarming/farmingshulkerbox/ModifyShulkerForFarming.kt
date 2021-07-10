1724bf060d76b66b62bbef248f82e002a1a9411e
SuL <jjw011202@naver.com>
2021-07-11 01:37:16 +0900

    셜커 죽을 시, 셜커 껍데기 드랍

--- src/main/kotlin/kr/sul/itemfarming/farmingshulkerbox/ModifyShulkerForFarming.kt
+++ src/main/kotlin/kr/sul/itemfarming/farmingshulkerbox/ModifyShulkerForFarming.kt
@@ -1,12 +1,15 @@
 package kr.sul.itemfarming.farmingshulkerbox
 
 import kr.sul.itemfarming.Main.Companion.plugin
+import kr.sul.servercore.util.ItemBuilder.nameIB
 import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.entity.*
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.*
+import org.bukkit.inventory.ItemStack
 import org.bukkit.potion.PotionEffectType
 
 object ModifyShulkerForFarming : Listener {
@@ -38,5 +41,11 @@ object ModifyShulkerForFarming : Listener {
                 }
             }, 200L)
         }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    fun onShulkerDeath(e: EntityDeathEvent) {
+        if (e.isCancelled) return
+        e.entity.location.world.dropItem(e.entity.location, ItemStack(Material.SHULKER_SHELL).nameIB("&7[ &f셜커껍데기 &7]"))
     }
 }
